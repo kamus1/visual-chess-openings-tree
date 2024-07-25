a2a425bde38761e5d3ea58bd1ef8c9517a9d0806
kamus1 <benjamincamus9@gmail.com>
2024-07-25 15:20:20 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+     <>
+      <h1>Visual Chess Openings Tree</h1>
+
+      <div className="card">
+        <p>
+          chessboard
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
